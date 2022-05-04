4f7b414db9c3b83076c4fe37a6f4c86816b6ef2d
Arya Kumar <aryak@WinjitTechnologiesPvtLtd.local>
2022-05-04 14:46:18 +0530

    updated api

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const expresss = require('express');
 const app = expresss();
 
-app.get('/', (req, res) => {
-    res.send("Welcome to homepage");
+app.get('/welcome', (req, res) => {
+    res.send("Welcome to nodejs beanstalk");
 });
+
+app.get('/', (req, res) => {
+    res.send("Welcome to nodejs");
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
